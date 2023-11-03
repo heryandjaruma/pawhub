081809195903dc3f2c51abab8875035693b77a18
JonathanLim <jonathan.lim002@binus.ac.id>
2023-11-03 11:25:41 +0700

    Update SDK into 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.pawhub"
-    compileSdk = 31
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.pawhub"
         minSdk = 24
-        targetSdk = 31
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
