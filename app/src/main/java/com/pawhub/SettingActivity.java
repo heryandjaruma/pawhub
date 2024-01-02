3ce00b1244f75834d114f2fba4f7eb57039b5c07
Chelsea Marchelle <135018058+chelseams@users.noreply.github.com>
2024-01-02 16:07:34 +0700

    SettingActivity

---
+++ app/src/main/java/com/pawhub/SettingActivity.java
@@ -0,0 +1,12 @@
+package com.pawhub;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
