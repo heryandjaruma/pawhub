31c0baf6bd8bd77ccdb0bfa1cff23e027a3c5f95
nayr <heryandjaruma@gmail.com>
2024-01-03 20:01:12 +0700

    add post skeleton added

---
+++ app/src/main/java/com/pawhub/AddPawgenicActivity.java
@@ -0,0 +1,14 @@
+package com.pawhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPawgenicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_pawgenic);
+    }
+}
--- app/src/main/java/com/pawhub/fragment/AddPhotoFragment.java
+++ app/src/main/java/com/pawhub/fragment/AddPhotoFragment.java
@@ -1,13 +1,18 @@
 package com.pawhub.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.pawhub.AddPawgenicActivity;
 import com.pawhub.R;
 
 /**
@@ -16,6 +21,8 @@ import com.pawhub.R;
  * create an instance of this fragment.
  */
 public class AddPhotoFragment extends Fragment {
+
+    private CardView add_pawgenic;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +68,22 @@ public class AddPhotoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_add_photo, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        add_pawgenic = view.findViewById(R.id.card_add_pawgenic);
+
+        add_pawgenic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), AddPawgenicActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
