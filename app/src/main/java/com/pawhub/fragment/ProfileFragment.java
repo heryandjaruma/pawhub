0c5879ebd9a0914c6e3f1d95883b79ef8668a8a7
nayr <heryandjaruma@gmail.com>
2024-01-03 19:48:05 +0700

    profile finish

--- app/src/main/java/com/pawhub/fragment/ProfileFragment.java
+++ app/src/main/java/com/pawhub/fragment/ProfileFragment.java
@@ -2,13 +2,24 @@ package com.pawhub.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.pawhub.R;
+import com.pawhub.implementation.UserRepositoryImpl;
+import com.pawhub.model.User;
+import com.pawhub.repository.UserRepository;
+import com.pawhub.utils.Callback;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +27,12 @@ import com.pawhub.R;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+    private TextView username_profile, post_count_profile;
+    private ImageView profile_picture;
+    private FirebaseUser authUser = FirebaseAuth.getInstance().getCurrentUser();
+    private UserRepository userRepository = new UserRepositoryImpl();
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -62,5 +79,30 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        username_profile = view.findViewById(R.id.username_in_profile);
+        post_count_profile = view.findViewById(R.id.post_count_profile);
+        profile_picture = view.findViewById(R.id.profile_picture);
+
+        userRepository.getThisUser(new Callback<User>() {
+            @Override
+            public void onSuccess(User result) {
+                username_profile.setText(result.getUsername());
+                post_count_profile.setText(result.getPost_count().toString());
+                Picasso.get().load(result.getProfile_picture()).into(profile_picture);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+
+            }
+        });
+
+
+
     }
 }
