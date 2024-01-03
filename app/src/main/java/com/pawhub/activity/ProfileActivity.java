0e5d6c4ef8fd065936c3709a2a6a825fa4f61f30
Chelsea Marchelle <135018058+chelseams@users.noreply.github.com>
2024-01-03 19:10:25 +0700

    SettingActivity2

---
+++ app/src/main/java/com/pawhub/activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.pawhub.activity;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.pawhub.R;
+
+
+public class ProfileActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
